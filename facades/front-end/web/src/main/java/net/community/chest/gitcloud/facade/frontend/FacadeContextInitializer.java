a497f485422a8531213587b244921d90334008d5
lgoldstein <lgoldstein@vmware.com>
2013-09-12 13:16:29 +0300

    Initial web front-end project

--- facades/front-end/web/src/main/java/net/community/chest/gitcloud/facade/frontend/FacadeContextInitializer.java
+++ facades/front-end/web/src/main/java/net/community/chest/gitcloud/facade/frontend/FacadeContextInitializer.java
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package net.community.chest.gitcloud.facade.backend;
+package net.community.chest.gitcloud.facade.frontend;
 
 import java.io.File;
 
---
+++ facades/front-end/web/src/main/java/net/community/chest/gitcloud/facade/frontend/FacadeEnvironmentInitializer.java
@@ -0,0 +1,47 @@
+/* Copyright 2013 Lyor Goldstein
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.community.chest.gitcloud.facade.frontend;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
+import net.community.chest.gitcloud.facade.AbstractEnvironmentInitializer;
+import net.community.chest.gitcloud.facade.ConfigUtils;
+
+import org.apache.commons.lang3.tuple.Pair;
+
+/**
+ * @author Lyor Goldstein
+ * @since Sep 12, 2013 1:07:22 PM
+ */
+public class FacadeEnvironmentInitializer extends AbstractEnvironmentInitializer {
+    public static final String  PROPS_FILE_NAME="gitcloud-frontend.properties";
+
+    public FacadeEnvironmentInitializer() {
+        super();
+    }
+
+    @Override
+    protected void extractConfigFiles(File confDir) {
+        // TODO use some automatic detection mechanism for "META-INF/conf"
+        extractConfigFiles(confDir,
+                Collections.singletonList(
+                        Pair.<String,Collection<String>>of("META-INF/" + ConfigUtils.CONF_DIR_NAME,
+                                Collections.unmodifiableList(
+                                        Arrays.asList(PROPS_FILE_NAME, "gitcloud-frontend-log4j.xml")))));
+    }
+}
