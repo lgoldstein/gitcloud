14173d7705ce8009ae387c0cd97528627e949f02
lgoldstein <lgoldstein@vmware.com>
2013-09-15 08:47:28 +0300

    Initial bridging code for GitController

--- facades/front-end/web/src/main/java/net/community/chest/gitcloud/facade/frontend/git/GitController.java
+++ facades/front-end/web/src/main/java/net/community/chest/gitcloud/facade/frontend/git/GitController.java
@@ -15,21 +15,33 @@
 package net.community.chest.gitcloud.facade.frontend.git;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 import java.util.TreeSet;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 
+import javax.net.ssl.HttpsURLConnection;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.collections15.ExtendedCollectionUtils;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.ExtendedArrayUtils;
 import org.apache.commons.lang3.ExtendedValidate;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.Validate;
 import org.apache.commons.logging.ExtendedLogUtils;
+import org.apache.commons.net.ssl.SSLUtils;
 import org.eclipse.jgit.http.server.GitSmartHttpTools;
 import org.eclipse.jgit.lib.Constants;
 import org.springframework.context.RefreshedContextAttacher;
@@ -73,13 +85,66 @@ public class GitController extends RefreshedContextAttacher {
                        + " redirected to " + url.toExternalForm());
         }
 
-        // TODO copy the "Accept" and "Accept-Encoding" header values from the original request
-        throw new UnsupportedOperationException(method + " " + req.getRequestURI() + "?" + req.getQueryString());
+        HttpURLConnection   conn=openTargetConnection(method, url, req);
+        try {
+            if (RequestMethod.POST.equals(method)) {
+                InputStream postData=req.getInputStream();
+                try {
+                    OutputStream    postTarget=conn.getOutputStream();
+                    try {
+                        long    cpyLen=IOUtils.copyLarge(postData, postTarget);
+                        if (logger.isTraceEnabled()) {
+                            logger.trace("serveRequest(" + method + ")[" + req.getRequestURI() + "][" + req.getQueryString() + "]"
+                                       + " copied " + cpyLen + " bytes to " + url.toExternalForm());
+                        }
+                    } finally {
+                        postTarget.close();
+                    }
+                } finally {
+                    postData.close();
+                }
+            }
+            
+            int statusCode=conn.getResponseCode();
+            if ((statusCode < HttpServletResponse.SC_OK) || (statusCode >= HttpServletResponse.SC_MULTIPLE_CHOICES)) {
+                String    rspMsg=conn.getResponseMessage();
+                logger.warn("serveRequest(" + method + ")[" + req.getRequestURI() + "][" + req.getQueryString() + "]"
+                          + " bad status code (" + statusCode + ")"
+                          + " on redirection to " + url.toExternalForm() + ": " + rspMsg);
+                rsp.sendError(statusCode, rspMsg);
+            } else {
+                rsp.sendError(statusCode);
+                
+                copyResponseHeadersValues(conn, rsp, "Content-type");
+
+                if (RequestMethod.GET.equals(method)) {
+                    InputStream rspData=conn.getInputStream();
+                    try {
+                        OutputStream    rspTarget=rsp.getOutputStream();
+                        try {
+                            long    cpyLen=IOUtils.copyLarge(rspData, rspTarget);
+                            if (logger.isTraceEnabled()) {
+                                logger.trace("serveRequest(" + method + ")[" + req.getRequestURI() + "][" + req.getQueryString() + "]"
+                                           + " copied " + cpyLen + " bytes from " + url.toExternalForm());
+                            }
+                        } finally {
+                            rspTarget.close();
+                        }
+                    } finally {
+                        rspData.close();
+                    }
+                }
+            }
+        } finally {
+            conn.disconnect();
+        }
     }
     
     private URL resolveTargetRepository(HttpServletRequest req) throws IOException {
-        String  op=Validate.notEmpty(req.getParameter("service"), "Missing service specification", ArrayUtils.EMPTY_OBJECT_ARRAY);
-        ExtendedValidate.isTrue(ALLOWED_SERVICES.contains(op), "Unsupported service: %s", op);
+        String  op=req.getParameter("service");
+        if (!StringUtils.isEmpty(op)) {
+            ExtendedValidate.isTrue(ALLOWED_SERVICES.contains(op), "Unsupported service: %s", op);
+        }
         
         String  uriPath=req.getPathInfo(), repoName=extractRepositoryName(uriPath);
         if (StringUtils.isEmpty(repoName)) {
@@ -88,10 +153,100 @@ public class GitController extends RefreshedContextAttacher {
                                                  new IllegalArgumentException("Failed to extract repo name from " + uriPath));
         }
 
-        String  query=Validate.notEmpty(req.getQueryString(), "No query value", ArrayUtils.EMPTY_OBJECT_ARRAY);
-        return new URL("http://localhost:8080/git-backend/git" + uriPath + "?" + query);
+        String  query=req.getQueryString();
+        if (StringUtils.isEmpty(query)) {
+            return new URL("http://localhost:8080/git-backend/git" + uriPath);
+        } else {
+            return new URL("http://localhost:8080/git-backend/git" + uriPath + "?" + query);
+        }
     }
     
+    private HttpURLConnection openTargetConnection(RequestMethod method, URL url, HttpServletRequest req) throws IOException {
+        HttpURLConnection   conn=(HttpURLConnection) url.openConnection();
+        if (conn instanceof HttpsURLConnection) {
+            HttpsURLConnection    https=(HttpsURLConnection) conn;
+            https.setHostnameVerifier(SSLUtils.ACCEPT_ALL_HOSTNAME_VERIFIER);
+            https.setSSLSocketFactory(SSLUtils.ACCEPT_ALL_FACTORY.create());
+        }
+
+        conn.setConnectTimeout((int) TimeUnit.SECONDS.toMillis(5L));    // TODO inject from configuration
+        conn.setReadTimeout((int) TimeUnit.SECONDS.toMillis(30L));    // TODO inject from configuration
+        conn.setRequestMethod(method.name());
+        
+        if (RequestMethod.POST.equals(method)) {
+            conn.setDoOutput(true);
+            copyRequestHeadersValues(req, conn, "Content-Type");
+        }
+
+        // TODO copy the "Accept" and "Accept-Encoding" header values from the original request
+        copyRequestHeadersValues(req, conn, "Accept", "Accept-Encoding");
+        return conn;
+    }
+
+    private Map<String,String> copyRequestHeadersValues(HttpServletRequest req, HttpURLConnection conn, String ... headers) {
+        return copyRequestHeadersValues(req, conn, ExtendedArrayUtils.asList(headers));
+    }
+    
+    private Map<String,String> copyRequestHeadersValues(HttpServletRequest req, HttpURLConnection conn, Collection<String> headers) {
+        if (ExtendedCollectionUtils.isEmpty(headers)) {
+            return Collections.emptyMap();
+        }
+        
+        Map<String,String>  hdrsValues=null;
+        for (String hdrName : headers) {
+            String  hdrValue=req.getHeader(hdrName);
+            if (StringUtils.isEmpty(hdrValue)) {
+                continue;
+            }
+            
+            conn.setRequestProperty(hdrName, hdrValue);
+            
+            if (hdrsValues == null) {
+                hdrsValues = new TreeMap<String,String>(String.CASE_INSENSITIVE_ORDER);
+            }
+            
+            hdrsValues.put(hdrName, hdrValue);
+        }
+        
+        if (hdrsValues == null) {
+            return Collections.emptyMap();
+        } else {
+            return hdrsValues;
+        }
+    }
+
+    private Map<String,String> copyResponseHeadersValues(HttpURLConnection conn, HttpServletResponse rsp, String ... headers) {
+        return copyResponseHeadersValues(conn, rsp, ExtendedArrayUtils.asList(headers));
+    }
+    
+    private Map<String,String> copyResponseHeadersValues(HttpURLConnection conn, HttpServletResponse rsp, Collection<String> headers) {
+        if (ExtendedCollectionUtils.isEmpty(headers)) {
+            return Collections.emptyMap();
+        }
+        
+        Map<String,String>  hdrsValues=null;
+        for (String hdrName : headers) {
+            String  hdrValue=conn.getHeaderField(hdrName);
+            if (StringUtils.isEmpty(hdrValue)) {
+                continue;
+            }
+            
+            rsp.setHeader(hdrName, hdrValue);
+            
+            if (hdrsValues == null) {
+                hdrsValues = new TreeMap<String,String>(String.CASE_INSENSITIVE_ORDER);
+            }
+            
+            hdrsValues.put(hdrName, hdrValue);
+        }
+        
+        if (hdrsValues == null) {
+            return Collections.emptyMap();
+        } else {
+            return hdrsValues;
+        }
+    }
+
     // TODO move this to some generic util location
     public static final String extractRepositoryName(String uriPath) {
         if (StringUtils.isEmpty(uriPath)) {
